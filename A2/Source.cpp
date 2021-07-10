c3e8018d65189c7b83442c9de7feca6fa158f3b3
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-07-10 22:40:31 +0300

    Update Source.cpp

--- A2/Source.cpp
+++ A2/Source.cpp
@@ -12,7 +12,7 @@ public:
 			std::cout << ">Press 5 to exchange Pound to USD\n";
 			std::cout << ">Press 0 to EXIT\n";
 	}
-	void exchange() {//function for the loop
+	void exchange() {//function for the loop.
 		do {
 			selection();//function summoning.
 			std::cin >> y;//input for selections.
